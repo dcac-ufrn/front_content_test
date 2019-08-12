59fc6d8c92a1f10ab2e221ab469575e79eccd1e1
Igor Santos <igorasantos@gmail.com>
2019-08-12 17:07:05 -0300

    Updates

--- src/pages/sobre.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () => (
-  <Layout>
-    <h1>About</h1>
-    <p>Seção que precisa de subseções.</p>
-  </Layout>
-)
